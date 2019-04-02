61723b529fca9dff747a2b5bfd41b5361e625818
Kimi Liang <kimi.liang@accentrix.com>
2019-04-02 18:10:25 +0800

    delete auth code

--- app/api/auth/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from flask import Blueprint
-from app.api import context_path
-
-auth_bp = Blueprint('auth', __name__, url_prefix='%s/auth' % context_path)
-
-from app.api.auth import auth_api
